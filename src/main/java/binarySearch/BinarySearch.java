aa4625b644a15c11acde9f91730766be6b0db2da
kun.zhang <kun.zhang@liulishuo.com>
2019-05-23 22:13:26 +0800

    数组重复的数

--- src/main/java/binarySearch/BinarySearch.java
+++ src/main/java/binarySearch/BinarySearch.java
@@ -1,5 +1,20 @@
 package binarySearch;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BinarySearch {
-    
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 4, 3, 2, 6, 7};
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < arr.length ; i ++) {
+            if (map.get(arr[i]) != null)
+            {
+                System.out.println(arr[i]);
+            }
+            map.put(arr[i], i);
+        }
+    }
 }
