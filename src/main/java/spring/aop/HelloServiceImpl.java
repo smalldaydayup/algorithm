9c36619dc20759c115dd11b277293a4fb19eed94
kun.zhang <kun.zhang@liulishuo.com>
2019-05-30 21:19:48 +0800

    Spring aop

--- src/main/java/spring/aop/HelloServiceImpl.java
+++ src/main/java/spring/aop/HelloServiceImpl.java
@@ -6,4 +6,15 @@ public class HelloServiceImpl implements HelloService {
     public void sayHelloworld() {
         System.out.println("hello world!");
     }
+
+    public static void main(String[] args) {
+        MethodInvocation logTask = () -> System.out.println("log task start");
+        HelloServiceImpl helloService = new HelloServiceImpl();
+
+        Advice beforeAdvice = new BeforeAdvice(helloService, logTask);
+
+        HelloService helloServiceImplProxy = (HelloService) SimpleAop.getProxy(helloService, beforeAdvice);
+
+        helloServiceImplProxy.sayHelloworld();
+    }
 }
