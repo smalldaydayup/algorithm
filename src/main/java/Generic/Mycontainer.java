383757ae68e35249ac915bbd0a45f04ab32a831e
kun.zhang <kun.zhang@liulishuo.com>
2020-02-03 23:19:03 +0800

    feature_ssss

--- src/main/java/Generic/Mycontainer.java
+++ src/main/java/Generic/Mycontainer.java
@@ -19,6 +19,26 @@ public class Mycontainer<T extends Animal> {
 
 
         Life life1 = new Human();
-        life1.say();
+        life1.say(); 
+
+        int[] arr = new int[]{1,2,3,45};
+        for (int i: arr) {
+            System.out.println(i);
+        }
+
+        ArrayList<String> st = new ArrayList<>();
+        st.add("s1");
+        st.add("s2");
+        st.add("s3");
+        st.add("s4");
+        st.add("s5");
+        st.add("s6");
+        st.add("s7");
+
+        Object[] array = st.toArray();
+        for (Object i: array) {
+            System.out.println(i);
+        }
+
     }
 }
--- src/main/java/sword/TwoDimensionSearch.java
+++ src/main/java/sword/TwoDimensionSearch.java
@@ -4,7 +4,7 @@ public class TwoDimensionSearch {
 
     public static void main(String[] args) {
         int[][] arr = new int[][]{{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
-        boolean ret = contain(14, arr);
+        boolean ret = contain(15, arr);
         System.out.println(ret);
     }
 
